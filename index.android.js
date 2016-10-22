32d7253c56964d910aa21fbecfb785b30b7c986a
adityareddy <p.aditya.reddi@gmail.com>
2016-10-22 10:19:38 +0530

    add react native

---
+++ index.android.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import {Admin} from './src';
+
+export default class adminonrest extends Component {
+  render() {
+    return (
+      <View>
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('adminonrest', () => adminonrest);
